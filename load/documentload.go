cdfde94a631922e0bcebb495c8dab54c19680f48
FrancisSy <francis.sy98@gmail.com>
2025-03-15 22:06:12 -0500

    create documents

--- load/documentload.go
+++ load/documentload.go
@@ -14,7 +14,7 @@ func loadDocuments() {
 	// steps
 	// check number of documents in collection
 	// if 1M then do not continue
-	// else if less than 1M then delete
+	// else if less than 1M then clear collection
 	// else
 	// load creationStatsMap with document breakdown
 	// each document should have its own id (UUID)
@@ -26,6 +26,68 @@ func loadDocuments() {
 	// create documents and add to interface slice
 	// insert all documents using a transaction
 	util := dbutil.NewMongoDbUtil("")
+
+	var docs []model.Restaurant
+
+	// create 10k document batches with the same country
+	idx := 65
+	country := "COUNTRY " + string(byte(idx))
+	for i := 0; i < 1000000; i += 10000 {
+		for j := 0; j < 10000; j++ {
+			doc := restaurantSkeleton()
+			doc.Address.Country = country
+			docs = append(docs, doc)
+		}
+		idx++
+	}
+
+	// create 1k document batches with the same state
+	idx = 65
+	state := "STATE " + string(byte(idx))
+	for i := 0; i < 1000000; i += 1000 {
+		for j := 0; j < 1000; j++ {
+			doc := restaurantSkeleton()
+			doc.Address.State = state
+			docs = append(docs, doc)
+		}
+		idx++
+	}
+
+	// create 100 document batches with the same city
+	idx = 65
+	city := "CITY " + string(byte(idx))
+	for i := 0; i < 1000000; i += 100 {
+		for j := 0; j < 100; j++ {
+			doc := restaurantSkeleton()
+			doc.Address.City = city
+			docs = append(docs, doc)
+		}
+		idx++
+	}
+
+	// create 10 document batches with the same chef name
+	for i := 0; i < 1000000; i += 10 {
+		headChef := &model.Chef{
+			ChefId:     uuid.NewString(),
+			FirstName:  randString(16),
+			LastName:   randString(16),
+			Dob:        "00-00-0000",
+			IsHeadChef: true,
+		}
+		chef := &model.Chef{
+			ChefId:     uuid.NewString(),
+			FirstName:  randString(16),
+			LastName:   randString(16),
+			Dob:        "00-00-0000",
+			IsHeadChef: false,
+		}
+		for j := 0; j < 10; j++ {
+			doc := restaurantSkeleton()
+			doc.Chefs = append(doc.Chefs, *headChef)
+			doc.Chefs = append(doc.Chefs, *chef)
+			docs = append(docs, doc)
+		}
+	}
 
 	util.Close()
 }
